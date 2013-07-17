9f486bac4ad850f2e0eca5238bf2d0dae3598cc7
Bo Cheng <chengbo1983@gmail.com>
2013-07-17 10:40:30 -0700

    remove header and footer in article section

--- content.php
+++ content.php
@@ -1,15 +1,13 @@
 
     <article id="post-<?php the_ID(); ?>">
-        <header>
-            <?php the_post_thumbnail(); ?>
-            <?php if ( is_single() ) : ?>
-            <h1><?php the_title(); ?></h1>
-            <?php else : ?>
-            <h1>
-                <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( 'Permalink to %s', the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-            </h1>
-            <?php endif; ?>
-        </header>
+        <?php the_post_thumbnail(); ?>
+        <?php if ( is_single() ) : ?>
+        <h1><?php the_title(); ?></h1>
+        <?php else : ?>
+        <h1>
+            <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( 'Permalink to %s', the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+        </h1>
+        <?php endif; ?>
 
         <?php if ( is_search() ) : ?>
         <div>
@@ -21,7 +19,5 @@
         </div>
         <?php endif; ?>
 
-        <footer>
-            <?php edit_post_link(); ?>
-        </footer>
+        <?php edit_post_link(); ?>
     </article>
