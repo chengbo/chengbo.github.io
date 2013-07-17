340c655b14c0422c11f9c4dc4b776f41406e03ec
Bo Cheng <chengbo1983@gmail.com>
2013-07-17 15:52:32 -0700

    set a custom style to site title and subtitle

--- header.php
+++ header.php
@@ -21,8 +21,8 @@
 <div id="container">
 	<header id="header" role="banner">
 		<hgroup>
-			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2><?php bloginfo( 'description' ); ?></h2>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-subtitle"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
 
 		<nav id="site-navigation" role="navigation">
