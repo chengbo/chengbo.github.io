100a5d698f1242f14a1f76c20ad7d8d1d2992546
Bo Cheng <chengbo1983@gmail.com>
2013-07-17 10:41:51 -0700

    add a custom footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+
+        <footer role="contentinfo">
+            Inspiration from life is proudly powered by
+            <a href="http://wordpress.org/">WordPress</a>
+        </footer>
+    </div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
