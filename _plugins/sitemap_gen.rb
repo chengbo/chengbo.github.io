6dd3bc6a548229bf95d87228888676396c8cbd05
bo cheng <chengbo1983@gmail.com>
2013-08-26 22:18:54 -0700

    change to protocol-relative url

--- _plugins/sitemap_gen.rb
+++ _plugins/sitemap_gen.rb
@@ -40,7 +40,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://blog.chengbo.net"
+  MY_URL = "//blog.chengbo.net"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
